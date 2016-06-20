dbb273f2fa321be598438a48df44b52a5fded4a4
Zach <millikanzach@gmail.com>
2016-06-20 15:37:34 -0400

    adds ng-sumbit to form. adds ng-model to all form fields. adds Mutant constructor to the controller.

--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -8,6 +8,20 @@
   function MutantListController() {
     var vm = this;
 
+    vm.addMutant = addMutant;
     vm.mutants = ['deadpool', 'nightcrawler', 'gambit'];
+    vm.newMutant = new Mutant();
+
+    function Mutant() {
+      this.name = '';
+      this.phone = '';
+      this.topic = '';
+      this.notified = false;
+      this.complete = false;
+    }
+
+    function addMutant() {
+      vm.mutants.push(vm.newMutant);
+    }
   }
 })();
