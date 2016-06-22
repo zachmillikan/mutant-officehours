3d6ff70b024b4ec58746383511039991fc821b02
Zach <millikanzach@gmail.com>
2016-06-22 13:20:18 -0400

    configures register state for auth module.

--- app/auth/config.route.js
+++ app/auth/config.route.js
@@ -1,13 +1,18 @@
-// (function () {
-//   'use strict';
-//
-//   angular
-//     .module('mutantApp.auth')
-//     .config(configFunction);
-//
-//   configFunction.$inject = ['$stateProvider'];
-//
-//   function configFunction($stateProvider) {
-//
-//   }
-// })();
+(function () {
+  'use strict';
+
+  angular
+    .module('mutantApp.auth')
+    .config(configFunction);
+
+  configFunction.$inject = ['$stateProvider'];
+
+  function configFunction($stateProvider) {
+    $stateProvider.state('register', {
+      url: '/register',
+      templateUrl: 'app/auth/register.html',
+      controller: 'AuthController',
+      controllerAs: 'vm'
+    });
+  }
+})();
