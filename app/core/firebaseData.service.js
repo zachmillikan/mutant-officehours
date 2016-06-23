3941820a08df1bd973f758932ea05e6dd76f99d6
Zach <millikanzach@gmail.com>
2016-06-23 13:44:39 -0400

    Set up firebaseDataService and add script tag.

---
+++ app/core/firebaseData.service.js
@@ -0,0 +1,17 @@
+(function() {
+  'require strict';
+
+  angular
+    .module('mutantApp.core')
+    .factory('firebaseDataService', firebaseDataService);
+
+  function firebaseDataService () {
+    var service = {};
+
+    return service;
+
+    //////////////////////
+
+    
+  }
+})();
