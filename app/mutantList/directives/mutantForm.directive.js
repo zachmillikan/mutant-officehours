c932114eda05cfe2572ba67b33b6a5cfa7125f44
Zach <millikanzach@gmail.com>
2016-06-28 14:42:18 -0400

    Add controller

--- app/mutantList/directives/mutantForm.directive.js
+++ app/mutantList/directives/mutantForm.directive.js
@@ -7,8 +7,28 @@
 
   function xtMutantForm () {
     return {
-      templateURL: 'app/mutantList/directives/mutantForm.html',
+      templateUrl: 'app/mutantList/directives/mutantForm.html',
       restrict: 'E',
+      controller: MutantFormController,
+      controllerAs: 'vm',
+      bindToController: true,
+      scope: {
+        mutants: '=',
+      },
     };
+  }
+
+  MutantFormController.$inject = ['mutantService'];
+
+  function MutantFormController (mutantService) {
+    var vm = this;
+
+    vm.addMutant = addMutant;
+    vm.newMutant = new mutantService.Mutant();
+
+    function addMutant() {
+      vm.mutants.$add(vm.newMutant);
+      vm.newMutant = mutantService.Mutant();
+    }
   }
 })();
