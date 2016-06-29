3b3df9cc46373212690518f8ffc3d798749665bb
Zach <millikanzach@gmail.com>
2016-06-29 15:29:43 -0400

    Refactored login/logout

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -5,10 +5,37 @@
     .module('mutantApp.auth')
     .controller('AuthController', AuthController);
 
-  AuthController.$inject = ['$state', 'authService', ];
+  AuthController.$inject = ['$state', 'authService'];
 
   function AuthController($state, authService) {
     var vm = this;
+
+    vm.register = register;
+    vm.login = login;
+    vm.error = null;
+
+    function register(user) {
+      return authService.register(user)
+        .then(function() {
+          vm.login(user);
+        })
+        .then(function() {
+          return authService.sendWelcomeEmail(user.email);
+        })
+        .catch(function(error) {
+          vm.error = error;
+        });
+    }
+
+    function login(user) {
+      return authService.login(user)
+        .then(function(user) {
+          $state.go('mutantList');
+        })
+        .catch(function(error) {
+          vm.error = error;
+        });
+    }
 
   }
 })();
--- app/auth/directives/auth.directive.js
+++ app/auth/directives/auth.directive.js
@@ -1,53 +1,31 @@
-(function () {
+(function() {
   'use strict';
 
   angular
-   .module('mutantApp.auth')
-   .directive('xtAuthForm', xtAuthForm);
+    .module('mutantApp.auth')
+    .directive('xtAuthForm', xtAuthForm);
 
   function xtAuthForm() {
     return {
       templateUrl: 'app/auth/directives/auth.html',
       restrict: 'E',
+      controller: AuthFormController,
+      controllerAs: 'vm',
+      bindToController: true,
+      scope: {
+        formName: '@',
+        submitFunction: '&',
+        error: '='
+      }
     };
   }
+  function AuthFormController() {
+    var vm = this;
 
-AuthFormController = ['$state', 'authService']
-
-function AuthFormController ($state, authService) {
-  var vm = this;
-
-  vm.register = register;
-  vm.login = login;
-  vm.error = null;
-
-  vm.user = {
-    email: '',
-    password: ''
+    vm.user = {
+      email: '',
+      password: ''
+    }
   }
 
-  function register(user) {
-    return authService.register(user)
-      .then(function() {
-        vm.login(user);
-      })
-      .then(function() {
-        return authService.sendWelcomeEmail(user.email);
-      })
-      .catch(function(error) {
-        vm.error = error;
-      });
-  }
-
-  function login(user) {
-    authService.login(user)
-      .then(function(user) {
-        $state.go('mutantList');
-      })
-      .catch(function(error){
-        vm.error = error;
-      });
-  }
-
-}
 })();
